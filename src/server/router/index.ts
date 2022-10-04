9610bb10b895423a9d7329cd854ea505139b8956
Guston-era <me@gustonera.co.ke>
2022-10-04 18:18:46 +0300

    merge app router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,7 +1,10 @@
 import { createRouter } from './context'
 import superjson from 'superjson'
+import { chatsRouter } from './chats-router'
 
-export const appRouter = createRouter().transformer(superjson)
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge('msg.', chatsRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
