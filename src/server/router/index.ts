10002ccfcd735c68d1a76051ca2d8a2d0e4bfbb0
Guston-era <me@gustonera.co.ke>
2022-10-04 17:00:30 +0300

    set up app router and utils

---
+++ src/server/router/index.ts
@@ -0,0 +1,7 @@
+import { createRouter } from './context'
+import superjson from 'superjson'
+
+export const appRouter = createRouter().transformer(superjson)
+
+// export type definition of API
+export type AppRouter = typeof appRouter
