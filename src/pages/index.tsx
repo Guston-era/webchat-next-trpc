cf9becfd68626bb8047c2b3557c12113a2a062d3
Guston-era <me@gustonera.co.ke>
2022-10-04 18:50:06 +0300

    add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardForm,
+  CardHeader,
+  List,
+  ListItem,
+} from '../components'
+// import { .... } from "@prisma/client";
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useCallback, useState, useEffect } from 'react'
@@ -29,7 +38,27 @@ const Home: NextPage = () => {
         <meta name="description" content="Chat app" />
       </Head>
 
-      <main></main>
+      <main>
+        <Card>
+          <CardContent>
+            <CardHeader title="Chat Room" listLength={list?.length ?? 0} />
+            {isLoading ? (
+              <span className="p-2">Hold on.. fetching chats...</span>
+            ) : (
+              <List>
+                {list?.map((item) => (
+                  <ListItem key={item._id.toString()} item={item} />
+                ))}
+              </List>
+            )}
+          </CardContent>
+          <CardForm
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+            submit={insertOne}
+          />
+        </Card>
+      </main>
     </>
   )
 }
