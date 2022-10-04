5cbc1119c82329ab74b93fd58671322a7796a4d4
Guston-era <me@gustonera.co.ke>
2022-10-04 18:23:46 +0300

    add components file

---
+++ src/components/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import React, { memo } from 'react'
+interface CardProps {
+  children: React.ReactNode
+}
+interface CardHeaderProps {
+  title: string
+  listLength: number
+}
+interface ChatMessage {
+  _id: object
+  title?: string
+  createdAt: string
+}
+interface CardFormProps {
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  submit: () => void
+}
